d122dfb6d3f2a8a6a170ebb7e2caa92217216b42
heathnapier <napierhb@miamioh.edu>
2017-02-28 19:30:30 -0500

    Update HelloWorldTest.java

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -1,12 +1,31 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class HelloWorldTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+public class TestHelloWorld {
 
+   private HelloWorld h;
+	
+   @Before
+   public void setUp() throws Exception 
+   {
+      h = new HelloWorld();
+   }
+
+   @Test
+   public void testHelloEmpty() 
+   {
+      assertEquals(h.getName(),"");
+      assertEquals(h.getMessage(),"Hello!");
+   }
+	
+   @Test
+   public void testHelloWorld() 
+   {
+      h.setName("World");
+      assertEquals(h.getName(),"World");
+      assertEquals(h.getMessage(),"Hello World!");
+   }
 }
